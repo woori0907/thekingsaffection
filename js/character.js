21776e608468350f6d0f6477c484743d74d318cc
Woori <rose5307@naver.com>
2021-11-25 00:38:58 +0900

    loading main character's profile complete

--- js/character.js
+++ js/character.js
@@ -1,17 +1,28 @@
 'use strict';
 
+const characterMenu = document.querySelector(".sidebar__menu");
 const characterImage = document.querySelector(".content__characterimg");
 const characterName = document.querySelector(".description__title");
 const characterActor = document.querySelector(".description__actor");
 const characterSubtitle = document.querySelector(".description__subtitle");
 const characterArticle = document.querySelector(".description__article");
 
-const renderProfile = () =>{
-    characterImage.style.backgroundImage = `url(${characterList[0].backgroundImage})`;
-    characterName.innerHTML = `<img src="${characterList[0].name}">`;
-    characterActor.innerText = `${characterList[0].actor}`;
-    characterSubtitle.innerText = `${characterList[0].subtitle}`;
-    characterArticle.innerText = `${characterList[0].article}`;
+const INIT_PROFILE_INDEX = 0;
+
+const renderProfile = (index) =>{
+    const currentIndex = index;
+
+    characterImage.style.backgroundImage = `url(${characterList[currentIndex].backgroundImage})`;
+    characterName.innerHTML = `<img src="${characterList[currentIndex].name}">`;
+    characterActor.innerText = `${characterList[currentIndex].actor}`;
+    characterSubtitle.innerText = `${characterList[currentIndex].subtitle}`;
+    characterArticle.innerText = `${characterList[currentIndex].article}`;
 }
 
-renderProfile();
+const clickMenu = (event) => {
+    renderProfile(event.target.getAttribute("data-index"));
+}
+
+renderProfile(INIT_PROFILE_INDEX);
+
+characterMenu.addEventListener('click', clickMenu);
